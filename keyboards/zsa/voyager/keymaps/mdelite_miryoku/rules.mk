62f72b14c6d51d38872fb4cef2dbc2ac1099acbc
Michael Doolittle <mgdoolittle@gmail.com>
2024-10-28 17:12:38 -0700

    setting defaults

--- keyboards/zsa/voyager/keymaps/mdelite_miryoku/rules.mk
+++ keyboards/zsa/voyager/keymaps/mdelite_miryoku/rules.mk
@@ -1,4 +1,4 @@
 # Copyright 2023 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-MIRYOKU_KLUDGE_THUMBCOMBOS=yes
+MIRYOKU_MAPPING=SHIFTED_ROWS
--- users/mdelite_miryoku/custom_rules.mk
+++ users/mdelite_miryoku/custom_rules.mk
@@ -1,3 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_ALPHAS=QWERTY
+MIRYOKU_TAP=QWERTY
+MIRYOKU_NAV=VI
+MIRYOKU_CLIPBOARD=WIN
